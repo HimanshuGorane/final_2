c6155d016db3aa02ce13328cfdc3a23a94f04240
HimanshuGorane <himanshug17082001@gmail.com>
2022-12-30 22:48:08 +0530

    solution 1

---
+++ script.js
@@ -0,0 +1,11 @@
+function showm(){
+    var h1 = document.getElementById('h1');
+    var h2 = document.getElementById('h2');
+    var h3 = document.getElementById('h3');
+
+    if (h1.value === '' | h2.value==='' | h3.value===''){
+        
+    }else {
+        document.getElementById('message').style.opacity = '1';
+    }
+}
